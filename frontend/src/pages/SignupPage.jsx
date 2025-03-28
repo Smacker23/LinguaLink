bf841e854126100226bdfb7c1891b2b847523387
Alexandre <133523544+Smacker23@users.noreply.github.com>
2025-03-28 15:30:16 +0000

    Update SignupPage.jsx

--- frontend/src/pages/SignupPage.jsx
+++ frontend/src/pages/SignupPage.jsx
@@ -66,7 +66,7 @@ function SignupPage() {
         e.preventDefault();
         //form_validation(); Implement also a frontend form validation ??
         try {
-            const response = await fetch("http://127.0.0.1:8000/api/signup", {
+            const response = await fetch(`${process.env.REACT_APP_BASE_API_URL}signup`, {
                 method: "POST",
                 headers: { "Content-Type": "application/json", },
                 body: JSON.stringify(formData),
